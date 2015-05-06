4d41863c7a3b3a002bee65262a68a37505d2b03d
Brett Vitaz <brett@vitaz.net>
2015-05-05 19:14:54 -0700

    [Server] Initial commit.

---
+++ server.py
@@ -0,0 +1,9 @@
+import bottle
+import waitress
+import controller
+
+
+if __name__ == '__main__':
+    bottle_app = bottle.app()
+    my_controller = controller.Controller(bottle_app)
+    waitress.serve(bottle_app, host='0.0.0.0', port=8080)
