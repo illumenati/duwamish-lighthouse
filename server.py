22ba81ee7bed81c3a1da4b8d2ace4c38a957b5dd
alan <alan@fancysandwiches.com>
2015-06-12 00:47:51 -0700

    Add scheduler. Schedule lights on for 9:00pm and lights off for 11:00pm

--- server.py
+++ server.py
@@ -2,10 +2,28 @@ import bottle
 import waitress
 import controller
 import breathe
+from pytz import timezone
+from apscheduler.schedulers.background import BackgroundScheduler
 
+bottle_app = bottle.app()
+scheduler = BackgroundScheduler()
+scheduler.configure(timezone=timezone('US/Pacific'))
+breather = breathe.Breathe()
+my_controller = controller.Controller(bottle_app, None)
+
+
+@scheduler.scheduled_job(trigger='cron', hour=21, minute=0)
+def on_job():
+    """Start at 9:00pm PT"""
+    print('STARTING BREATHER')
+    breather.restart()
+
+@scheduler.scheduled_job(trigger='cron', hour=23, minute=0)
+def off_job():
+    """End at 11:00pm PT"""
+    print("STOPPING BREATHER")
+    breather.shutdown()
 
 if __name__ == '__main__':
-    bottle_app = bottle.app()
-    breather = breathe.Breathe()
-    my_controller = controller.Controller(bottle_app, breather)
+    scheduler.start()
     waitress.serve(bottle_app, host='0.0.0.0', port=7000)
