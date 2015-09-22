6f6b0f2c8470e472cc4f4084980627a92482d8c5
Luke Swart <lukeswart@gmail.com>
2015-09-22 11:31:08 -0700

    Update lighthouse hours 7pm-9pm

--- server.py
+++ server.py
@@ -12,16 +12,16 @@ breather = breathe.Breathe()
 my_controller = controller.Controller(bottle_app, breather)
 
 
-@scheduler.scheduled_job(trigger='cron', hour=20, minute=0)
+@scheduler.scheduled_job(trigger='cron', hour=19, minute=0)
 def on_job():
-    """Start at 10:00pm PT"""
+    """Start at 7:00pm PT"""
     print('STARTING BREATHER')
     breather.restart()
 
 
-@scheduler.scheduled_job(trigger='cron', hour=22, minute=0)
+@scheduler.scheduled_job(trigger='cron', hour=21, minute=0)
 def off_job():
-    """End at 12:01am PT"""
+    """End at 9:00pm PT"""
     print("STOPPING BREATHER")
     breather.shutdown()
 
